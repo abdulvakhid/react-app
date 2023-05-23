66242eeb5982ca7f7c4f923e2b80eea120248e3f
abdulvakhid <buriyevabdulvohid055@gmail.com>
2023-05-23 15:04:37 +0500

    Add data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box } from "@mui/material";
+import { useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      hello world
-    </div>
-  );
+	const [hasMore, setHasMore] = useState(true);
+	const [data, setData] = useState(Array.from({ length: 50 }));
+
+	const fetchmoredata = () => {
+		setTimeout(() => {
+			setData(data.concat(Array.from({ length: 50 })));
+		}, 2000);
+	};
+
+	return (
+		<Box className="App">
+			<h1>React Infiite scroll</h1>
+			<InfiniteScroll
+				dataLength={data.length}
+				next={fetchmoredata}
+				hasMore={hasMore}
+				loader={<p style={{fontSize:"24px", color:"green", fontWeight:"bold"}}>Loading...</p>}>
+				{data.map((item, index) => {
+					return (
+						<Box
+							sx={{
+								border: "1px solid red",
+								mb: "25px",
+								padding: "10px",
+								width: "30%",
+								mx: "auto",
+							}}
+							key={index}>
+							{" "}
+							<h3 sx={{ fw: "bold",}}>#{index + 1} - element</h3>
+
+						</Box>
+					);
+				})}
+			</InfiniteScroll>
+		</Box>
+	);
 }
 
 export default App;
